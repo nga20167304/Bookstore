d7cf3fde880053f21e94bd762e4a762f64fbd56d
nga20167304 <nga.dt167304@gmail.com>
2019-09-04 00:54:58 +0700

    update website homepage to display the categories as menu items

--- src/com/bookstore/controller/admin/AdminHomeServlet.java
+++ src/com/bookstore/controller/admin/AdminHomeServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bookstore.controller.BaseServlet;
+
 @WebServlet("/admin/")
 public class AdminHomeServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
